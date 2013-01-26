4a19cd2f32eefc00e3a4860d5da721562fab042b
mridul <mridulv09@gmail.com>
2013-01-26 19:28:12 +0530

    Added y.tab.h

---
+++ y.tab.h
@@ -0,0 +1,110 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     EQ_COMP=258,
+     UNEQ=259,
+     LT=260,
+     LE=261,
+     GT=262,
+     GE=263,
+     IN=264,
+     IS=265,
+     PLUS_GYM=266,
+     MINUS_SYM=267,
+     MULTIPLY_SYM=268,
+     DIVIDE_SYM=269,
+     DIV=270,
+     MOD=271,
+     OR_SYM=272,
+     AND_SYM=273,
+     TILDA=274,
+     ASSIGN=275,
+     REPEAT=276,
+     UNTIL=277,
+     FOR=278,
+     TO=279,
+     BY=280,
+     DOCASE=281,
+     END=282,
+     WHILE=283,
+     IF_COND=284,
+     THEN=285,
+     ELSEIF=286,
+     CASE_COND=287,
+     OF=288,
+     STRLEN=289,
+     STRCAT=290,
+     STRCPY=291,
+     STRREV=292,
+     STRCMP=293,
+     CHAR=294,
+     INT=295,
+     REAL=296,
+     LONGREAL=297,
+     STRING=298,
+     IDENT=299
+   };
+#endif
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 9 "assignment_9.y"
+
+  int ival;
+  float fval;
+  char *sval;
+       
+
+
+/* Line 1676 of yacc.c  */
+#line 82 "y.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
